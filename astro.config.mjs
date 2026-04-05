89b343cf3c0566a83eb6cf4c0831949d4f2c591e
royprince12-code <royprinceveigas031@gmail.com>
2026-04-05 21:02:17 +0530

    Update files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://github.com/royprince12-code/"
+    site: "https://royprince12-code.github.io",
+    base: "/EcoVex_Sjec"
 });
